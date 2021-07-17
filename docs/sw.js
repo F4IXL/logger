5d22ccf28da9da1d2ae2cd942cb103a56679ad31
Franco Bouly <fbouly@gmail.com>
2021-07-17 16:27:11 +0200

    Add build for GH pages test

---
+++ docs/sw.js
@@ -0,0 +1,141 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  const singleRequire = name => {
+    if (name !== 'require') {
+      name = name + '.js';
+    }
+    let promise = Promise.resolve();
+    if (!registry[name]) {
+      
+        promise = new Promise(async resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = name;
+            document.head.appendChild(script);
+            script.onload = resolve;
+          } else {
+            importScripts(name);
+            resolve();
+          }
+        });
+      
+    }
+    return promise.then(() => {
+      if (!registry[name]) {
+        throw new Error(`Module ${name} didn’t register its module`);
+      }
+      return registry[name];
+    });
+  };
+
+  const require = (names, resolve) => {
+    Promise.all(names.map(singleRequire))
+      .then(modules => resolve(modules.length === 1 ? modules[0] : modules));
+  };
+  
+  const registry = {
+    require: Promise.resolve(require)
+  };
+
+  self.define = (moduleName, depsNames, factory) => {
+    if (registry[moduleName]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    registry[moduleName] = Promise.resolve().then(() => {
+      let exports = {};
+      const module = {
+        uri: location.origin + moduleName.slice(1)
+      };
+      return Promise.all(
+        depsNames.map(depName => {
+          switch(depName) {
+            case "exports":
+              return exports;
+            case "module":
+              return module;
+            default:
+              return singleRequire(depName);
+          }
+        })
+      ).then(deps => {
+        const facValue = factory(...deps);
+        if(!exports.default) {
+          exports.default = facValue;
+        }
+        return exports;
+      });
+    });
+  };
+}
+define("./sw.js",['./workbox-600da2ae'], function (workbox) { 'use strict';
+
+  /**
+  * Welcome to your Workbox-powered service worker!
+  *
+  * You'll need to register this file in your web app.
+  * See https://goo.gl/nhQhGp
+  *
+  * The rest of the code is auto-generated. Please don't update this file
+  * directly; instead, make changes to your Workbox build configuration
+  * and re-run your build process.
+  * See https://goo.gl/2aRDsh
+  */
+
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting();
+    }
+  });
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+
+  workbox.precacheAndRoute([{
+    "url": "assets/index.c81ace9d.js",
+    "revision": "d017dc8a58115deeccb3e79dc895c7d9"
+  }, {
+    "url": "assets/index.f138fde1.css",
+    "revision": "07829f225755199d1089e5ddd31b5af1"
+  }, {
+    "url": "assets/vendor.002cea8f.js",
+    "revision": "a224de3ceb88ae8ecf0d56df7151dafe"
+  }, {
+    "url": "icons/check.svg",
+    "revision": "479b1e9c9ee75d513fc27d0f3d208c1d"
+  }, {
+    "url": "icons/logo.png",
+    "revision": "00043be3e7b6e6c50deb4cb8bd35c217"
+  }, {
+    "url": "icons/logo.svg",
+    "revision": "048e5522d7c99e7a11e93b0deb8dfe15"
+  }, {
+    "url": "index.html",
+    "revision": "0ebcff977887545b14ce78d588b55c5e"
+  }, {
+    "url": "icons/logo.png",
+    "revision": "00043be3e7b6e6c50deb4cb8bd35c217"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "16101b0eeb08bc7cd48fd320394d31d2"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+});
+//# sourceMappingURL=sw.js.map
